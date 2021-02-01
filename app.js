e6d4de9436b9fc0aa917cefb38aeade9c3518334
Rushil Mistry <rushilmistry99@gmail.com>
2021-01-31 22:22:58 -0600

    grid is ready

--- app.js
+++
@@ -1,7 +0,0 @@
-const menu = documents.querySelector('#mobile-menu')
-const menulinks = document.querySelector('.navbar__menu')
-
-menu.addEventListener('click', function(e) {
-    menu.classlist.toggle('is-active');
-    menulinks.classlist.toggle('active');
-});
